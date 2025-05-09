0774283b4d971cd066453157e3d0eef218305efa
GabryTarchi <vintage.lele@gmail.com>
2025-05-09 17:12:14 +0200

    implementati i service

--- src/main/java/com/example/esercizioColloquio/service/UserService.java
+++ src/main/java/com/example/esercizioColloquio/service/UserService.java
@@ -1,11 +1,13 @@
 package com.example.esercizioColloquio.service;
 
 import com.example.esercizioColloquio.dto.UserDTO;
+import com.example.esercizioColloquio.entity.User;
 import com.example.esercizioColloquio.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,22 +21,46 @@ public class UserService {
     }
 
     public List<UserDTO> findAll(){
-        return null;
+        List<UserDTO> response = new ArrayList<>();
+        List<User> userList = userRepository.findAll();
+
+        for(User user : userList){
+            UserDTO userDTO = new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getEmail(), user.getAddress());
+            response.add(userDTO);
+        }
+        return response;
     }
 
     public UserDTO findById(int id){
-        return null;
+        User user = userRepository.findById(id).orElseThrow();
+        return new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getEmail(), user.getAddress());
     }
 
     public UserDTO create(UserDTO userDTO){
-        return null;
+        User user = new User();
+
+        user.setId(userDTO.id());
+        user.setName(userDTO.name());
+        user.setSurname(userDTO.surname());
+        user.setEmail(userDTO.email());
+        user.setAddress(userDTO.address() );
+        userRepository.save(user);
+
+        return new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getEmail(), user.getAddress());
     }
 
-    public void update(int id, UserDTO userDTO){
+    public void update(int id, UserDTO request){
+        User updatedUser = new User();
+        updatedUser.setId(id);
+        updatedUser.setName(request.name());
+        updatedUser.setSurname(request.surname());
+        updatedUser.setEmail(request.email());
+        updatedUser.setAddress(request.address());
 
+        userRepository.save(updatedUser);
     }
 
     public void delete(int id){
-
+        userRepository.deleteById(id);
     }
 }
