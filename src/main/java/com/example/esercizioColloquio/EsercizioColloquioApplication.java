61576497c874bbd512bdb7674e619bce87361d15
GabryTarchi <vintage.lele@gmail.com>
2025-05-09 15:06:39 +0200

    primo commit

---
+++ src/main/java/com/example/esercizioColloquio/EsercizioColloquioApplication.java
@@ -0,0 +1,13 @@
+package com.example.esercizioColloquio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioColloquioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioColloquioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esercizioColloquio/EsercizioColloquioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizioColloquio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioColloquioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
