539e47062ee73e28d5a6fd55e9177fb46fa51be2
GabryTarchi <vintage.lele@gmail.com>
2025-05-09 16:16:57 +0200

    creati e implementati User e UserDTO

--- src/main/java/com/example/esercizioColloquio/dto/UserDTO.java
+++ src/main/java/com/example/esercizioColloquio/dto/UserDTO.java
@@ -1,4 +1,23 @@
 package com.example.esercizioColloquio.dto;
 
-public record UserDTO() {
+import com.example.esercizioColloquio.entity.User;
+
+public record UserDTO(
+        Integer id,
+        String name,
+        String surname,
+        String email,
+        String address
+
+
+) {
+    public User toEntity(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.id);
+        user.setEmail(userDTO.email);
+        user.setName(userDTO.name);
+        user.setSurname(userDTO.surname);
+        user.setAddress(userDTO.address);
+        return user;
+    }
 }
---
+++ src/main/java/com/example/esercizioColloquio/entity/User.java
@@ -0,0 +1,60 @@
+package com.example.esercizioColloquio.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String address;
+
+    // Getters and Setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
